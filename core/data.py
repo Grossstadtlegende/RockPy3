f86cb85f529a4ac2e3fa2c38c066fabf6f3ad4b0
Mike Volk <grossstadtlegende@gmail.com>
2016-01-04 18:12:38 +0100

    started find peak function

--- core/data.py
+++ core/data.py
@@ -1564,6 +1564,29 @@ class RockPyData(object):
 
         return slope, sigma, y_intercept, x_intercept
 
+    def find_peaks(self, column_name, delta, variable='variable', derivative=False):
+        """
+        Converted from MATLAB script at http://billauer.co.il/peakdet.html
+        Adapted to RockPy ba mike volk
+
+        Parameters
+        ----------
+            column_name: str
+                the column_name of the y data where the peak is looked for
+            delta: float
+                minimal difference in variable between peaks
+                from original documentation:
+                    % A point is considered a maximum peak if it has the maximal
+                    % value, and was preceded (to the left) by a value lower by
+                    % DELTA.
+                    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+                    % This function is released to the public domain; Any use is allowed.
+        Returns two arrays
+
+
+
+        """
+
     def derivative(self, dependent_var, independent_var, smoothing=0):
         """
         calculate d( independent_var) / d( dependent_var)
