cf585bdf13b1ce05dea03eda44cbf6e6ba70a30d
Michael Volk <grossstadtlegende@gmail.com>
2016-01-12 15:15:52 -0600

    lines longer

--- Packages/Mag/Features/day.py
+++ Packages/Mag/Features/day.py
@@ -206,10 +206,10 @@ def sd_sp_93nm(ax, **plt_opt):
 
 
 def day_grid(ax, **options):
-    ax.hlines(0.5, 0, 10)
-    ax.hlines(0.1, 0, 10)
-    ax.vlines(5, 0, 0.6)
-    ax.vlines(1.5, 0, 0.6)
+    ax.hlines(0.5, 0, 15)
+    ax.hlines(0.1, 0, 15)
+    ax.vlines(5, 0, 1)
+    ax.vlines(1.5, 0, 1)
     ax.text(0.6, 0.55, 'SD', fontdict={'size': 18, 'color': '#909090'})
     ax.text(2.6, 0.3, 'PSD', fontdict={'size': 18, 'color': '#909090'})
     ax.text(5.9, 0.05, 'MD', fontdict={'size': 18, 'color': '#909090'})
