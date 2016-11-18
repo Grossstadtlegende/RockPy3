101cec6e359455d157c8c0e48c72e30d4d40da54
Mike Volk <grossstadtlegende@gmail.com>
2016-11-18 17:32:03 +0100

    endmember unmixing added

--- core/calculate.py
+++ core/calculate.py
@@ -1,12 +1,14 @@
 import numpy as np
 import scipy.optimize
 
-def endmember_unmixing(data, end_members=2, maxiter=1000):
 
+def endmember_unmixing(data, end_members=2, maxiter=1000):
     """
     End-member unmixing adapted from:
 
-    Heslop, D., and M. Dillon (2007), Unmixing magnetic remanence curves without a priori knowledge, Geophysical Journal International, 170(2), 556–566, doi:10.1111/j.1365-246X.2007.03432.x.
+    Heslop, D., and M. Dillon (2007), Unmixing magnetic remanence curves without a priori knowledge,
+    Geophysical Journal International, 170(2), 556–566, doi:10.1111/j.1365-246X.2007.03432.x.
+
     Parameters
     ----------
     data: ndarray
@@ -38,15 +40,15 @@ def endmember_unmixing(data, end_members=2, maxiter=1000):
     l = data.shape[1]
     m = end_members  # end members
     n = data.shape[0]
-    eps = 1e-15 # no div/0
+    eps = 1e-15  # no div/0
 
     # initialize the matrixes with random numbers
     A = np.random.random_sample((n, m))
     S = np.random.random_sample((m, l))
 
-    #calculation
+    # calculation
     for i in range(maxiter):
         S = S * np.dot(A.T, X) / (np.dot(A.T, np.dot(A, S)) + eps)
         A = A * np.dot(X, S.T) / (np.dot(A, np.dot(S, S.T)) + eps)
 
-    return A, S, np.dot(A,S), X-np.dot(A,S)
+    return A, S, np.dot(A, S), X - np.dot(A, S)
